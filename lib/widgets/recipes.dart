bcbd221f7be6f15a9b1da9122434420206ae7a5d
Stephan Bakkelund Valois <stephan@devalo.no>
2020-08-26 20:39:14 +0200

    can see content

--- lib/widgets/recipes.dart
+++ lib/widgets/recipes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:matbit/widgets/app.dart';
 
 class Recipes extends StatefulWidget {
   @override
@@ -8,6 +9,37 @@ class Recipes extends StatefulWidget {
 class _RecipesState extends State<Recipes> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Matbit'),
+      ),
+      body: _buildBody(context),
+    );
   }
+}
+
+Widget _buildBody(BuildContext context) {
+  return _buildList(context, dummySnapshot);
+}
+
+Widget _buildList(BuildContext context, List<Map> snapshot) {
+  return ListView(
+    children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+  );
+}
+
+Widget _buildListItem(BuildContext context, Map data) {
+  var _test = data.values.toList();
+
+  print(_test);
+  print(data["name"]);
+  return Padding(
+    padding: EdgeInsets.symmetric(),
+    child: Container(
+      child: ListTile(
+        title: Text(data["name"]),
+        onTap: () => print(data["name"]),
+      ),
+    ),
+  );
 }
